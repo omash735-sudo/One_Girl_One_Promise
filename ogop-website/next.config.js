4d27d80c9675230d02b3e19f441c75a99f64d0d2
omash735-sudo <omash735@gmail.com>
2026-08-09 01:18:35 +0200

    Update next.config.js

--- ogop-website/next.config.js
+++ ogop-website/next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com'],
+    domains: [
+      'images.unsplash.com',
+      'via.placeholder.com',
+      'res.cloudinary.com'
+    ],
   },
   experimental: {
     serverActions: true,
   },
+  staticPageGenerationTimeout: 120,
 }
 
 module.exports = nextConfig
